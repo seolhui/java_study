86f23119da052a5617e64a150722b7725ec46586
seolhui.park <seolhui.park@saramin.co.kr>
2019-08-16 18:00:38 +0900

    test commit

---
+++ src/Ticket.java
@@ -0,0 +1,19 @@
+public class Ticket {
+    final static public Ticket EMPTY = new Ticket(null);
+    final private Theater theater;
+    private boolean isEntered = false;
+    public Ticket(Theater theater){
+        this.theater = theater;
+    }
+    public boolean isValid(Theater theater){
+        if(isEntered || theater != this.theater || this == EMPTY){
+            return false;
+        }else{
+            isEntered = true;
+            return true;
+        }
+    }
+    public Long getFee(){
+        return theater.getFee();
+    }
+}
